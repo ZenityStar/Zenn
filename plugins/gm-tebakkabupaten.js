1b1cdb1eb87a0f6bfe30ddc9656871986230cd24
ZenityStar <113663049+ZenityStar@users.noreply.github.com>
2022-09-19 14:45:36 +0700

    Update gm-tebakkabupaten.js

--- plugins/gm-tebakkabupaten.js
+++ plugins/gm-tebakkabupaten.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 let timeout = 120000
-let poin = 500
+let poin = 100
 let handler = async (m, { conn, usedPrefix }) => {
   conn.tebakkabupaten = conn.tebakkabupaten ? conn.tebakkabupaten : {}
   let id = m.chat
@@ -29,5 +29,5 @@ Bonus: ${poin} XP
 handler.help = ['tebakkabupaten']
 handler.tags = ['game']
 handler.command = /^tebakkabupaten/i
-
+handler.limit = true
 module.exports = handler
