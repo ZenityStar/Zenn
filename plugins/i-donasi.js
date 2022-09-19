9a05b70055616d61fef442f49b758a68ed585c84
ZenityStar <113663049+ZenityStar@users.noreply.github.com>
2022-09-19 14:36:52 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,13 +8,12 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • Triii [62895385157376]
-│ • Gopay  [62895385157376]
-│ • Dana  [62895385157376]
+│ • Triii [6285607831412]
+│ • Gopay  [6285607831412]
+│ • Dana  [6285607831412]
 │ • Qris  [Chat OWNER]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6289522724388
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
